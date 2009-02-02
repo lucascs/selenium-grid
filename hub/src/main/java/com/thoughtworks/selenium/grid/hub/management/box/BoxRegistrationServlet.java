1ec4dc68dbaf751c4299f8150c70bebf79395034
lucascs <lucascs@caelum-laptop.(none)>
2009-02-02 19:30:35 -0200

    Starting Box feature, intended to mean the machines where Remote Controls are run.
    BoxRegistrationServlet will register such boxes

---
+++ hub/src/main/java/com/thoughtworks/selenium/grid/hub/management/box/BoxRegistrationServlet.java
@@ -0,0 +1,42 @@
+package com.thoughtworks.selenium.grid.hub.management.box;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.thoughtworks.selenium.grid.hub.management.RegistrationManagementServlet;
+
+/**
+ * Servlet used by Selenium Remote Control to register themselves to the grid.
+ *
+ * @author Philippe Hanrigou
+ */
+public class BoxRegistrationServlet extends RegistrationManagementServlet {
+
+    private static final Log LOGGER = LogFactory.getLog(BoxRegistrationServlet.class);
+	private final BoxPool pool;
+	private final File boxFile;
+
+    public BoxRegistrationServlet(BoxPool pool, File boxFile) {
+		this.pool = pool;
+		this.boxFile = boxFile;
+	}
+
+	@Override
+	protected void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        LOGGER.info("Registering new Box...");
+        Box box = new Box(request.getParameter("host"), Integer.parseInt(request.getParameter("port")));
+        pool.register(box);
+        pool.saveTo(boxFile);
+        LOGGER.info("Registered " + box);
+        writeSuccessfulResponse(response);
+    }
+
+
+}
