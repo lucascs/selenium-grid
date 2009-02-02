d90c0ce9d77c1f94169d90c8484fd568e300f3f8
lucascs <lucascs@caelum-laptop.(none)>
2009-02-02 19:36:14 -0200

    registering new servlets on HubServer

--- hub/src/main/java/com/thoughtworks/selenium/grid/hub/HubServer.java
+++ hub/src/main/java/com/thoughtworks/selenium/grid/hub/HubServer.java
@@ -1,14 +1,21 @@
 package com.thoughtworks.selenium.grid.hub;
 
-import com.thoughtworks.selenium.grid.configuration.HubConfiguration;
-import com.thoughtworks.selenium.grid.hub.management.RegistrationServlet;
-import com.thoughtworks.selenium.grid.hub.management.UnregistrationServlet;
-import com.thoughtworks.selenium.grid.hub.management.LifecycleManagerServlet;
-import com.thoughtworks.selenium.grid.hub.management.console.ConsoleServlet;
+import java.io.File;
+
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.handler.ContextHandlerCollection;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
+
+import com.thoughtworks.selenium.grid.configuration.HubConfiguration;
+import com.thoughtworks.selenium.grid.hub.management.LifecycleManagerServlet;
+import com.thoughtworks.selenium.grid.hub.management.RegistrationServlet;
+import com.thoughtworks.selenium.grid.hub.management.UnregistrationServlet;
+import com.thoughtworks.selenium.grid.hub.management.box.BoxPool;
+import com.thoughtworks.selenium.grid.hub.management.box.BoxRegistrationServlet;
+import com.thoughtworks.selenium.grid.hub.management.box.BoxStartupServlet;
+import com.thoughtworks.selenium.grid.hub.management.box.Pinger;
+import com.thoughtworks.selenium.grid.hub.management.console.ConsoleServlet;
 
 /**
  * Self contained Selenium Grid Hub. Uses Jetty to as a standalone web application.
@@ -30,12 +37,24 @@ public class HubServer {
         root = new Context(contexts, "/", Context.SESSIONS);
 //        root.setResourceBase("./");
 //        root.addHandler(new ResourceHandler());
+        BoxPool pool = new BoxPool();
+        File boxFile = new File("boxes.xml");
+        if (boxFile.exists()) {
+			pool.loadFrom(boxFile);
+		}
+        
         root.addServlet(new ServletHolder(new HubServlet()), "/selenium-server/driver/*");
-        root.addServlet(new ServletHolder(new ConsoleServlet()), "/console");
+        root.addServlet(new ServletHolder(new ConsoleServlet(pool)), "/console");
         root.addServlet(new ServletHolder(new RegistrationServlet()), "/registration-manager/register");
         root.addServlet(new ServletHolder(new UnregistrationServlet()), "/registration-manager/unregister");
         root.addServlet(new ServletHolder(new LifecycleManagerServlet()), "/lifecycle-manager");
 
+        root.addServlet(new ServletHolder(new BoxRegistrationServlet(pool, boxFile)), "/box-registration-manager/register");
+        root.addServlet(new ServletHolder(new BoxStartupServlet(pool)), "/box-registration-manager/start");
+
+        Thread thread = new Thread(new Pinger(pool));
+        thread.setDaemon(false);
+		thread.start();
         server.start();
         server.join();
     }
