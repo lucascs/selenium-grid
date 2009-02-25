78b404e5781debfc719ff58d6a547aa974922266
lucascs <lucascs@caelum-laptop.(none)>
2009-02-24 22:52:21 -0300

    test for BoxRegistrationServlet

---
+++ hub/src/test/unit/java/com/thoughtworks/selenium/grid/hub/management/box/BoxRegistrationServletTest.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.selenium.grid.hub.management.box;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jbehave.classmock.UsingClassMock;
+import org.jbehave.core.mock.Mock;
+import org.junit.Test;
+
+public class BoxRegistrationServletTest extends UsingClassMock {
+
+	@Test
+	public void testRegisteringABoxWillPutItInBoxPoolAndWriteTheFile() throws Exception {
+		Mock pool = mock(BoxPool.class);
+		File file = new File(".");
+		
+		BoxRegistrationServlet servlet = new BoxRegistrationServlet((BoxPool) pool, file);
+
+		Mock request = mock(HttpServletRequest.class);
+		request.expects("getParameter").with("host").will(returnValue("localhost"));
+		request.expects("getParameter").with("port").will(returnValue("1234"));
+		pool.expects("register").with(eq(new Box("localhost", 1234)));
+		pool.expects("saveTo").with(sameInstanceAs(file));
+		
+		Mock response = mock(HttpServletResponse.class);
+		response.stubs("getWriter").will(returnValue(mock(PrintWriter.class)));
+		servlet.process((HttpServletRequest)request, (HttpServletResponse) response);
+		
+		verifyMocks();
+	}
+}
